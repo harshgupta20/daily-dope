c5862c2f0896cc7368f5b27ae5357cc0312b305d
Harsh Gupta <59175412+harshgupta20@users.noreply.github.com>
2025-02-23 20:10:21 +0530

    removed unwanted files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,19 @@
-import React, { useEffect, useState } from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AddTask from './pages/user/AddTask';
-import ViewTasks from './pages/user/ViewTasks';
 import Home from './pages/Home';
 import { AuthContext } from './context/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute'; // Import ProtectedRoute
 import { ToastContainer } from 'react-toastify';
 import NotFound from './pages/NotFound';
-import VoteTask from './pages/worker/VoteTask';
+import { useEffect, useState } from 'react';
 
 const App = () => {
   const [isUserAuthenticated, setIsUserAuthenticated] = useState({});
 
   useEffect(() => {
     const token = localStorage.getItem("token");
-    const address = localStorage.getItem("address");
     if (token) {
-      setIsUserAuthenticated({ address, token });
+      setIsUserAuthenticated(token);
     }
   }, []);
 
@@ -29,11 +25,9 @@ const App = () => {
           <Routes>
             <Route element={<Home />} path="/" />
             {/* Protected routes */}
-            <Route element={<ProtectedRoute element={<AddTask />} />} path="/user/add-task" />
-            <Route element={<ProtectedRoute element={<ViewTasks />} />} path="/user/view-tasks" />
+            {/* <Route element={<ProtectedRoute element={<VoteTask />} />} path="/worker/vote" /> */}
 
 
-            <Route element={<ProtectedRoute element={<VoteTask />} />} path="/worker/vote" />
             <Route element={<NotFound />} path="*" />
           </Routes>
 
